b34aa25c0df6c05ccc1ba133d645cb3c747e2a2a
Mohamed Elemam <mohamed.elemam.younis@gmail.com>
2023-12-16 00:35:03 +0200

    Implement API functions for recipe fetching

---
+++ src/Component/GenerateSection/GenerateSection.tsx
@@ -0,0 +1,111 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import MealDetails from "../MealDetails/MealDetails";
+import { ApiData } from "./types";
+
+const kitchens: string[] = [
+  "American",
+  "British",
+  "Canadian",
+  "Chinese",
+  "French",
+  "Indian",
+  "Italian",
+];
+const GenerateSection = () => {
+  const [KitchenMeals, setKitchenMeals] = useState<ApiData[]>([]);
+  const [mealData, setMealData] = useState<ApiData | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [apiError, setApiError] = useState<string>("");
+
+  async function getKitchenMeals(kitchenStr: string) {
+    try {
+      setLoading(true);
+      const { data } = await axios.get(
+        import.meta.env.VITE_API_LINK_GET_COUNTRY_MEALS + kitchenStr
+      );
+      setKitchenMeals(data.meals);
+    } catch (error: unknown) {
+      setApiError(
+        "There was a problem fetching the meal data. Please try again later."
+      );
+    } finally {
+      setLoading(false);
+    }
+  }
+  async function getDish(mealStr: string) {
+    try {
+      setLoading(true);
+      const { data } = await axios.get(
+        import.meta.env.VITE_API_LINK_GET_MEAL_DETAILS + mealStr
+      );
+      setMealData(data.meals[0]);
+    } catch (error: unknown) {
+      setApiError(
+        "There was a problem fetching the meal data. Please try again later."
+      );
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    getKitchenMeals("Egyptian");
+  }, []);
+
+  function getRandomMeal() {
+    const number: number = Math.floor(
+      Math.random() * (KitchenMeals?.length - 1) || 0
+    );
+    const dish = KitchenMeals[number]?.strMeal;
+    getDish(dish);
+  }
+
+  return (
+    <>
+      <section id="section2" className="generate-section">
+        <div className="layer">
+          <div className="flex flex-col gap-5 justify-center items-center text-center container py-5">
+            <h3 className="my-3 text-white text-4xl font-semibold">
+              Start discovering delicious recipes
+            </h3>
+            <p className="text-white text-3xl font-semibold">
+              Choose Your Favorite Food Area
+            </p>
+            <select
+              className="select select-warning w-full max-w-xs"
+              onChange={(e) => getKitchenMeals(e.target.value)}
+            >
+              <option selected>Egyptian</option>
+              {kitchens.map((kitchen, index) => (
+                <option key={index} value={kitchen}>
+                  {kitchen}
+                </option>
+              ))}
+            </select>
+            {apiError && (
+              <p className="text-error-content text-center">{apiError}</p>
+            )}
+            {loading ? (
+              <button className="btn btn-warning">
+                <span className="loading loading-spinner">loading</span>
+              </button>
+            ) : (
+              <button
+                onClick={() => {
+                  getRandomMeal();
+                }}
+                className="btn btn-warning cursor-pointer"
+              >
+                Get Recipe 🍖
+              </button>
+            )}
+          </div>
+          {mealData ? <MealDetails mealData={mealData} /> : null}
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default GenerateSection;
--- src/Component/MealDetails/MealDetails.tsx
+++ src/Component/MealDetails/MealDetails.tsx
@@ -1,143 +1,95 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { ApiData } from "../GenerateSection/types";
 
-const MealDetails = () => {
-  type ApiData = {
-    strMealThumb: string;
-    strMeal: string;
-    strInstructions: string;
-    strArea: string;
-    strCategory: string;
-    strSource: string;
-    strYoutube: string;
-  };
-  const [apiData, setApiData] = useState([]);
-  const [apiError, setApiError] = useState<unknown>();
-  const [dishData, setDishData] = useState<ApiData>();
-  const [randomMeal, setRandomMeal] = useState<ApiData>();
-  const [loading, setLoading] = useState<boolean>(false);
+const MealDetails = ({ mealData }: { mealData: ApiData }) => {
+  const recipeIngredients: string[] = [];
+  const recipeMeasurements: string[] = [];
 
-  async function getKitchenMeals(kitchenStr: string) {
-    try {
-      setLoading(true);
-      const { data } = await axios.get(
-        import.meta.env.VITE_API_LINK_GET_COUNTRY_MEALS + kitchenStr
-      );
-      setApiData(data.meals);
-      console.log(data.meals);
-    } catch (error: unknown) {
-      console.log(error);
-      setApiError(error);
-    } finally {
-      setLoading(false);
+  for (const key in mealData) {
+    if (
+      Object.prototype.hasOwnProperty.call(mealData, key) &&
+      key.startsWith("strIngredient")
+    ) {
+      const ingredient = mealData[key];
+      if (ingredient && typeof ingredient === "string") {
+        recipeIngredients.push(ingredient);
+      }
     }
-  }
-  async function getDish(mealStr: string) {
-    try {
-      setLoading(true);
-      const { data } = await axios.get(
-        import.meta.env.VITE_API_LINK_GET_MEAL_DETAILS + mealStr
-      );
-      setDishData(data.meals[0]);
-      console.log(data.meals);
-    } catch (error: unknown) {
-      console.log(error);
-    } finally {
-      setLoading(false);
+
+    if (
+      Object.prototype.hasOwnProperty.call(mealData, key) &&
+      key.startsWith("strMeasure")
+    ) {
+      const measurement = mealData[key];
+      if (measurement && typeof measurement === "string") {
+        recipeMeasurements.push(measurement);
+      }
     }
-  }
-
-  useEffect(() => {
-    getKitchenMeals("British");
-
-    // getDishs();
-  }, []);
-
-  const kitchens: string[] = [
-    "American",
-    "British",
-    "Canadian",
-    "Chinese",
-    "Egyptian",
-    "French",
-    "Indian",
-    "Italian",
-  ];
-
-  function getRandomMeal() {
-    const number: number = Math.floor(Math.random() * (apiData?.length - 1));
-    const dish = apiData[number]?.strMeal;
-    getDish(dish);
   }
 
   return (
     <>
-      <div className="flex flex-col gap-5 justify-center items-center text-center container ">
-        <h3>Feeling hungry</h3>
-        <p></p>
-        <details className="dropdown">
-          <summary className="m-1 btn">open or close</summary>
-          <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
-            {kitchens.map((kitchen, index) => (
-              <li key={index}>
-                <a>{kitchen}</a>
-              </li>
-            ))}
-          </ul>
-        </details>
-        {loading ? (
-          <button className="btn btn-warning">
-            <span className="loading loading-spinner"></span>
-          </button>
-        ) : (
-          <button
-            onClick={() => {
-              getRandomMeal();
-            }}
-            className="btn btn-warning"
-          >
-            Get Meal 🍖
-          </button>
-        )}
-      </div>
-      {/* <p className="text-warning">{apiError?}</p> */}
-      {dishData?.strMeal}
-   <div className="col-md-4">
-<div className=" rounded-4">
-  <div className=" meal-card">
-    <img
-      src={dishData?.strMealThumb}
-      className="w-75 my-3 rounded-2"
-    />
+      {mealData ? (
+        <div className="container">
+          <div>
+            <div className=" text-center text-white text-3xl font-bold">
+              <h1> {mealData?.strMeal} </h1>
 
-    <h1> {dishData?.strMeal} </h1>
-  </div>
-</div>
-</div>
-<div className="col-md-8">
-<h2>Instructions</h2>
-<p> {dishData?.strInstructions}</p>
-<div>
-  <span className="h2">Area :</span>
-  <span className="h4"> {dishData?.strArea}</span>
-</div>
-<div>
-  <span className="h2">Category :</span>
-  <span className="h4"> {dishData?.strCategory}</span>
-</div>
-<h2>Recipes :</h2>
-
-<a href={dishData?.strSource} className="btn btn-success me-3">
-  Source
-</a>
-<a href={dishData?.strYoutube} className="btn btn-danger me-3">
-  Youtube
-</a>
-</div> 
+              {mealData?.strMealThumb.length ? (
+                <img
+                  src={mealData?.strMealThumb}
+                  alt={mealData?.strMeal}
+                  className="md:w-1/4 w-1/2 my-3 rounded-md mx-auto"
+                />
+              ) : (
+                <div className="skeleton md:w-1/4 w-1/2 md:h-[320px] h-[300px]  my-3 rounded-md mx-auto"></div>
+              )}
+            </div>
+          </div>
+          <div className="flex flex-col md:flex-row gap-3">
+            <div className="md:min-w-[45%]">
+              <h2 className="text-warning text-2xl font-semibold my-2">
+                Recipes :
+              </h2>
+              <ul>
+                {recipeIngredients.map((ingredient, index) => (
+                  <li
+                    key={index}
+                    className="p-2 text-white font-semibold space-y-0"
+                  >
+                    {recipeMeasurements[index]} of {ingredient}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="md:min-w-[45%]">
+              <h2 className="text-warning text-2xl font-semibold my-2">
+                Instructions
+              </h2>
+              <p className="p-2 text-white font-semibold">
+                {mealData?.strInstructions}
+              </p>
+            </div>
+          </div>
+          <div className=" py-4 mx-auto text-center">
+            <a
+              href={mealData?.strSource}
+              className="btn btn-outline btn-warning me-3"
+              target="_blank"
+            >
+              Source
+            </a>
+            <a
+              href={mealData?.strYoutube}
+              className="btn btn-outline btn-error me-3"
+              target="_blank"
+            >
+              Youtube
+            </a>
+          </div>
+        </div>
+      ) : null}
     </>
   );
 };
 
 export default MealDetails;
-
-
