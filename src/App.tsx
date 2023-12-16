3d01fd79ad12b126afa69104130cd84bb6ebd6e4
Mohamed Elemam <mohamed.elemam.younis@gmail.com>
2023-12-16 22:03:57 +0200

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import HeroSection from "./Component/HeroSection/HeroSection";
 import GenerateSection from "./Component/GenerateSection/GenerateSection";
 import Footer from "./Component/Footer/Footer";
-import Navbar from './Component/Navbar/Navbar';
+import Navbar from "./Component/Navbar/Navbar";
 
 function App() {
   return (
     <>
       <Navbar />
-      <HeroSection />
       <GenerateSection />
       <Footer />
     </>
